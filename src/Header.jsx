2dc2e80cba3d976f5522aa0f8d671043aeda2b29
Sai Vikas Gopal <GopalSaiVikas@JohnDeere.com>
2023-06-20 15:34:50 -0400

    vite poc with charts

--- src/Header.jsx
+++ src/Header.jsx
@@ -9,7 +9,7 @@ export const Header = () => {
             <AppBar position="static">
                 <Toolbar>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        ROCKWELL AUTOMATION
+                        Fancy Automation LLC
                     </Typography>
                 </Toolbar>
             </AppBar>
